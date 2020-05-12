ec9743b9208217edcecfca931b8f50c9d17d35c4
NPospolita <nikita7asics@gmail.com>
2020-05-12 11:22:16 +0300

    init

---
+++ src/test/java/com/rbkmoney/shumaichvalidator/ShumaichValidatorApplicationTest.java
@@ -0,0 +1,16 @@
+package com.rbkmoney.shumaichvalidator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ShumaichValidatorApplication.class)
+public class ShumaichValidatorApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
